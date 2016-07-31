4e2197fdb68df15ddbbfe0984644c160ed677e5a
JavierChames <haimch@gmail.com>
2016-07-31 19:45:13 +0300

    Delete incorrect files

--- showmymovies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pass_sql="pass.txt"
-pass_sql=$(cat "$pass_sql")
-mysql -u root -p$pass_sql  -e 'select strPath,lastPlayed from movie_view where playCount>0 order by lastPlayed\G' MyVideos93 
-
-#mysql -u root -p$pass_sql  -e 'select strPath,lastPlayed from movie_view where playCount>0 order by lastPlayed\G' MyVideos93 | grep Path | awk -F ':' '{print$2}' | awk -F '/' '{print$5}'
-
--- showmyseries.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pass_sql="pass.txt"
-pass_sql=$(cat "$pass_sql")
-mysql -u root -p$file_pass  -e 'select strFilename,lastPlayed from episode_view where playCount>0 order by lastPlayed\G' MyVideos93
