1a5e9817b0f9169ac0df72007bf94be3b7211d0e
JavierChames <haimch@gmail.com>
2016-09-23 16:15:46 +0300

    Fix of movies and series

--- showmyseries.sh
+++ showmyseries.sh
@@ -2,4 +2,4 @@
 DB="MyVideos93"
 pass_sql="/home/pi/scripts/pass.txt"
 pass_sql=$(cat "$pass_sql")
-mysql -u root -p$pass_sql  -e 'select strFilename,lastPlayed from episode_view where playCount>0 order by lastPlayed\G' $DB
+mysql -u root -p$pass_sql  -e 'select strFilename as "File name",lastPlayed as "Watched date" from episode_view where playCount>0 order by lastPlayed\G' $DB
